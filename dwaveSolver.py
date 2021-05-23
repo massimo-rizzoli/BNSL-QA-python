90deaec9dc704a3b5ee9966ea1d0eac2fef43b4f
Massimo Rizzoli <massimo.rizzoli@studenti.unitn.it>
2021-05-23 18:54:00 +0200

    exact solver test with dimod to check the encoding

--- dwaveSolver.py
+++ dwaveSolver.py
@@ -2,8 +2,37 @@ import torch
 from QUBOMatrix import calcQUBOMatrix
 from solverUtils import getPath, getExpectedSolution, printInfoResults, getPath
 
+from dimod import BinaryQuadraticModel, Vartype
+from dimod.reference.samplers import ExactSolver
+
 def dwaveQA(Q, indexQUBO, posOfIndex, n):
-  pass
+  qubo = {}
+  for i in range(len(indexQUBO)):
+    for j in range(i,len(indexQUBO)):
+      if Q[i][j] != 0:
+        qubo[(indexQUBO[i],indexQUBO[j])] = Q[i,j].item()
+  print()
+  print(qubo)
+
+  #bqm = BinaryQuadraticModel(linear,quadratic,offset,Vartype.BINARY)
+  bqm = BinaryQuadraticModel.from_qubo(qubo)
+
+  sampler = ExactSolver()
+  response = sampler.sample(bqm)
+  print(response.first)
+
+  minXt = torch.zeros(len(indexQUBO))
+  for index, value in response.first.sample.items():
+    pos = posOfIndex[index]
+    minXt[pos] = value
+
+  minX = minXt.view(-1,1)
+  minY = torch.matmul(torch.matmul(minXt,Q),minX).item()
+
+  #with open('dimodOut.txt', 'w') as file:
+    #file.write(str(response))
+
+  return minXt, minY
 
 def main():
   path = getPath()
